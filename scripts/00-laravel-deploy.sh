905a3156b841a904422dc32dcc200c26464bb084
franklin <elendufranklin66@gmail.com>
2025-06-25 12:25:28 +0100

    create write location

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,6 +5,9 @@ composer install --no-dev --working-dir=/var/www/html
 echo "Caching config..."
 php artisan config:cache
 
+echo "creating write location"
+touch touch /tmp/database.sqlite
+
 echo "Running npm"
 npm install 
 
