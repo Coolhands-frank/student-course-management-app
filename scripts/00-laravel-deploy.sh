d1715cbc81d6c2e1abff88b8d3a74ae4faee8d9e
franklin <elendufranklin66@gmail.com>
2025-06-27 14:11:11 +0100

    docker fix

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+echo "📦 Running composer..."
+composer install --no-dev --working-dir=/var/www/html
+
+echo "⚙ Caching config..."
+php artisan config:cache
+
+echo "📁 Fixing permissions..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
+echo "🚦 Caching routes..."
+php artisan route:cache
+
 echo "🧬 Running migrations..."
 php artisan migrate --force
 
